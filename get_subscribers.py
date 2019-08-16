b684c0ed9044e510be79c94ab02c1bcb272899d5
rdimitrieva <rumyanamm@gmail.com>
2019-08-15 18:38:33 -0700

    ManyChat project changes

--- get_subscribers.py
+++ get_subscribers.py
@@ -2,7 +2,7 @@ import requests
 from requests import HTTPError
 from datetime import datetime, timedelta
 from apscheduler.schedulers.blocking import BlockingScheduler
-from static_data import CUSTOM_FIELD_ID, MESSAGE_TO_SEND
+from static_data import CUSTOM_FIELD_ID, MESSAGE_TO_SEND, CUSTOM_FIELD_VALUE, CUSTOM_FIELD_NAME
 import auth_token
 import send_message
 
@@ -15,6 +15,12 @@ def convert(s):
     return datetime.strptime(s, '%Y-%m-%dT%H:%M:%S%z')
 
 
+def get_custom_field_id():
+    response = requests.get("https://api.manychat.com/fb/page/getCustomFields", headers=header)
+    pass
+
+
+
 def get_subscribers_last30min(subscribers_dict):
     send_data_to = set()
     for sub in subscribers_dict:
@@ -25,9 +31,9 @@ def get_subscribers_last30min(subscribers_dict):
     return send_data_to
 
 
-def get_today_subscribers():
+def get_new_subscribers():  # subscribers with CustomField = "999999999"
     today = datetime.today().strftime('%Y-%m-%d')
-    params = {"field_id": CUSTOM_FIELD_ID, "field_value": today}
+    params = {"field_id": CUSTOM_FIELD_ID, "field_value": CUSTOM_FIELD_VALUE}
     response = requests.get("https://api.manychat.com/fb/subscriber/findByCustomField", params=params, headers=header)
     if response:
         return response.json()
@@ -38,17 +44,18 @@ def get_today_subscribers():
 
 def start_script():
     try:
-        subs = get_today_subscribers()
+        subs = get_new_subscribers()
     except HTTPError:
         scheduler.remove_job('my_job_id')
     else:
-        send_message.send_message_to_subscribers(MESSAGE_TO_SEND, get_subscribers_last30min(subs))
+        subscribed_last_30min = get_subscribers_last30min(subs)
+        send_message.send_message_to_subscribers(MESSAGE_TO_SEND, subscribed_last_30min)
+        send_message.change_custom_field_value(subscribed_last_30min)
 
 
 scheduler = BlockingScheduler()
 scheduler.add_job(start_script, 'interval', minutes=30, id='my_job_id')
 scheduler.start()
-
 
 
 
--- send_message.py
+++ send_message.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import auth_token
+from static_data import CUSTOM_FIELD_NAME
 
 
 auth_token = auth_token.AUTH_TOKEN
@@ -27,6 +28,25 @@ def send_message_to_subscribers(msg, subscribers_list):
         response = requests.post(url, data=json.dumps(payload), headers=headers)
         if response:
             print(f'Message sent to subscriber with ID: {subscriber}')
+        else:
+            print(f'An error has occurred: {response.status_code}')
+
+
+def change_custom_field_value(subscribers_list):
+    url = "https://api.manychat.com/fb/subscriber/setCustomFieldByName"
+    headers = {'Authorization': 'Bearer ' + auth_token,
+               'Accept': 'application/json',
+               'Content-Type': 'application/json'}
+
+    for subscriber in subscribers_list:
+        payload = {
+            "subscriber_id": subscriber,
+            "field_name": CUSTOM_FIELD_NAME,
+            "field_value": "000000000"
+        }
+        response = requests.post(url, data=json.dumps(payload), headers=headers)
+        if response:
+            print(f'Custom filed changed subscriber with ID: {subscriber}')
         else:
             print(f'An error has occurred: {response.status_code}')
 
